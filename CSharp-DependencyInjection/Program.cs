8fb402694df910ba2927cfafdf06f0ce2580e729
varunatgithub <90926513+varunatgithub@users.noreply.github.com>
2024-03-10 18:02:30 +0530

    Add project files.

---
+++ CSharp-DependencyInjection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharp_DependencyInjection
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
