be408be65e8a27fbeb4527fe81d38d12b63e574d
varunatgithub <90926513+varunatgithub@users.noreply.github.com>
2024-03-10 18:54:15 +0530

    Update and rename IMessegeSender.cs to IMessageSender.cs

--- CSharp-DependencyInjection/IMessageSender.cs
+++ CSharp-DependencyInjection/IMessageSender.cs
@@ -1,6 +1,6 @@
 ﻿namespace CSharp_DependencyInjection
 {
-public interface IMessegeSender
+public interface IMessageSender
 {
     // This is the contract that any message sender must follow
     void SendMessage(string message, string recipient);
