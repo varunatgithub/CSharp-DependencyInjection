3b49b0cc624e33f1288d8fccd6225f30da1ef781
varunatgithub <90926513+varunatgithub@users.noreply.github.com>
2024-03-10 18:50:43 +0530

    Update IMessegeSender.cs

--- CSharp-DependencyInjection/IMessegeSender.cs
+++ CSharp-DependencyInjection/IMessegeSender.cs
@@ -1,7 +1,8 @@
 ﻿namespace CSharp_DependencyInjection
 {
-    public interface IMessegeSender
-    {
-
-    }
+public interface IMessegeSender
+{
+    // This is the contract that any message sender must follow
+    void SendMessage(string message, string recipient);
+}
 }
